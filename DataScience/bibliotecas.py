38c4efbeb3e7a3361a84986694933ae729995cbf
Matheus Duarte <100427903+MatheuwsDuarte@users.noreply.github.com>
2025-12-24 01:15:19 -0300

    #10

---
+++ DataScience/bibliotecas.py
@@ -0,0 +1,11 @@
+import numpy
+import pandas
+import jupyter_client
+import matplotlib
+
+print(f"Versao do numpy: {numpy.__version__}")
+print(f"Versao do pandas: {pandas.__version__}")
+print(f"Versao do jupyter: {jupyter_client.__version__}")
+print(f"Versao do matplotlib: {matplotlib.__version__}")
+
+
